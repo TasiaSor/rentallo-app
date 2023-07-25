a861fc4ce91fe2f9a5f8c18fc37f872e5f8f5d82
Tetiana Sorochenko <tetiana.sorochenko@ukr.net>
2023-07-25 23:43:26 +0200

    [change] - /cars endpoint implemented with dtos -2

--- src/main/java/com/sda/rentalloapp/controller/CarRestController.java
+++ src/main/java/com/sda/rentalloapp/controller/CarRestController.java
@@ -6,33 +6,31 @@ import com.sda.rentalloapp.mapper.CarMapper;
 import com.sda.rentalloapp.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@@ -15,15 +17,20 @@
+@RestController
+@Slf4j
+@RequestMapping("/api")
 public class CarRestController {
 
     private final CarService carService;
     private final CarMapper carMapper;
 
-    public CarRestController(CarService carService) {
     public CarRestController(CarService carService, CarMapper carMapper) {
-            this.carService = carService;
-            this.carMapper = carMapper;
-        }
+        this.carService = carService;
+        this.carMapper = carMapper;
+    }
 
-        @GetMapping("/cars")
-        public List<Car> allCars() {
-            public List<CarDto> allCars() {
-                log.info("all cars request");
+    @GetMapping("/cars")
+    public List<CarDto> allCars() {
+        log.info("all cars request");
 
-                return carService.getAllCars();
-                return carService.getAllCars()
-                        .stream()
-                        .map(car -> carMapper.fromEntityToDto(car))
-                        .toList();
-            }
-        }
+        return carService.getAllCars()
+                .stream()
+                .map(car -> carMapper.fromEntityToDto(car))
+                .toList();
+    }
+}
