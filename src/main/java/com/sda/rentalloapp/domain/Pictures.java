f89cb41225261ef7beae53999229492fb250e14b
Tetiana Sorochenko <tetiana.sorochenko@ukr.net>
2023-07-26 19:17:58 +0200

    [change] - PicturesMapper implemented

--- src/main/java/com/sda/rentalloapp/domain/Pictures.java
+++ src/main/java/com/sda/rentalloapp/domain/Pictures.java
@@ -3,20 +3,20 @@ package com.sda.rentalloapp.domain;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
+@Builder
 public class Pictures {
 
     String mainPictureUrl;
     @ElementCollection
     List<String> picturesUrls;
-
 }
