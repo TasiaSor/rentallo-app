449a10d0b438122fc65150bb4cab8c330cc4d82a
Tetiana Sorochenko <tetiana.sorochenko@ukr.net>
2023-07-30 22:40:56 +0200

    [add] - endpoint for new cars - in progress

--- src/main/java/com/sda/rentalloapp/controller/CarRestControllerAdvisor.java
+++ src/main/java/com/sda/rentalloapp/controller/CarRestControllerAdvisor.java
@@ -4,6 +4,7 @@ import com.sda.rentalloapp.dto.ResponseDto;
 import com.sda.rentalloapp.exeption.WrongCarIdException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,13 +15,10 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 @Slf4j
 public class CarRestControllerAdvisor {
-
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(WrongCarIdException.class)
     public ResponseDto handleCarNotFoundException(WrongCarIdException exc) {
-        String path = ServletUriComponentsBuilder.fromCurrentRequest()
-                .build()
-                .getPath();
+        String path = getCurrentRequestPath();
 
 
         return ResponseDto.builder()
@@ -31,4 +29,25 @@ public class CarRestControllerAdvisor {
                 .path(path)
                 .build();
     }
+
+    private static String getCurrentRequestPath() {
+        return ServletUriComponentsBuilder.fromCurrentRequest()
+                .build()
+                .getPath();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseDto handleBadRequest(MethodArgumentNotValidException exc) {
+        String path = getCurrentRequestPath();
+
+        return ResponseDto.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .path(path)
+                .message(exc.getMessage())
+                .build();
+    }
+
 }
