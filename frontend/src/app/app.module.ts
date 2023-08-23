dc7028ab4a634f53dd52309687e700bea8b97590
Tetiana Sorochenko <tetiana.sorochenko@ukr.net>
2023-08-23 10:30:46 +0200

    [add] - top bar component created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent
+    CarsComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
