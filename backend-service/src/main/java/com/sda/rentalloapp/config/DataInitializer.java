ee1d24975d70be0cda94148f0fc2bc27a1daebaa
Tetiana Sorochenko <tetiana.sorochenko@ukr.net>
2023-08-27 23:13:14 +0200

    [fix] - address test city added

--- backend-service/src/main/java/com/sda/rentalloapp/config/DataInitializer.java
+++ backend-service/src/main/java/com/sda/rentalloapp/config/DataInitializer.java
@@ -46,6 +46,7 @@ public class DataInitializer implements CommandLineRunner {
 
             Address bieszczady = Address.builder()
                     .street("Bieszczadzka")
+                    .city("Lublin")
                     .zipCode("01-123")
                     .houseNumber("11")
                     .voivodeship("Podkarpackie")
